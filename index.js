bd5f7a3f07dd634fb5eff3cfc6b2b37257825828
TomasNieto7 <tnieto07@alumnos.uaq.mx>
2024-05-15 08:25:18 -0600

    app

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const db = require("./config/database.js")
 const app = express()
 
 const port = 3000
 
+app.use(express.json())
+app.use(express.json(express.urlencoded({
+    extended: true
+})))
+
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res, next) => {
-    console.log(req)
-    res.send("Hello World!")
+    res.render('index')
 })
 
-app.listen(port, ()=>{
+app.get("/pokemon", async (req, res, next) => {
+    const pokemon = await db.query("SELECT * FROM POKEMON;")
+    res.json(pokemon);
+});
+
+app.post("/pokemon", (req, res, next) => {
+    console.log(req.body);
+    res.send("Informacion guardada! (de a mentis)")
+})
+
+app.listen(port, () => {
     console.log(`Server is running in port ${port}`);
 })
